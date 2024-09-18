30f1158d0b093812c426e4b8190279d547f91022
Work <Work@MSI>
2024-09-18 16:24:04 -0500

    Where we ended on September 18 - resume with skimming through struct slides

--- September18_2024-COSC1437.002/September18_2024-COSC1437.002.cpp
+++ September18_2024-COSC1437.002/September18_2024-COSC1437.002.cpp
@@ -1,11 +1,15 @@
-
 #include <iostream>
 #include <vector>
+
+#include<iomanip>
 
 using std::cout; 
 using std::vector;
 using std::string; 
 
+
+using std::left; 
+using std::setw;
 //vector<int> nums = { 1, 2, 3, 4, 5 }; 
 
 /*
@@ -17,6 +21,8 @@ struct Person
 	bool hasHair; 
 	double weight; //lbs or kgs (160 lbs)
 	string eyeColor; 
+
+	int numberOfBodiesInBackyard;
 };
 
 struct RPGCharacter
@@ -31,6 +37,28 @@ struct RPGCharacter
 //
 //};
 
+void printListOfPeople(const vector<Person>& people)
+{
+
+	const int COL_WIDTH = 10; 
+
+	cout << left << setw(10) << "Has Hair: "; 
+	cout << left << setw(10) << "Weight:";
+	cout << left << setw(10) << "Eye color:"; 
+	cout << left << setw(10) << "Num bodies: ";
+	cout << "\n";
+		
+	for (const auto& person : people)
+	{
+		cout << std::boolalpha;
+		cout << left << setw(10) << person.hasHair;
+		cout << left << setw(10) << person.weight;
+		cout << left << setw(10) << person.eyeColor;
+		cout << left << setw(10) << person.numberOfBodiesInBackyard;
+
+		cout << "\n";
+	}
+}
 
 int main()
 {
@@ -48,13 +76,33 @@ int main()
 	//cout << names[0] << "\n";
 	
 	//an example of creating a `Person` "object" 
-	Person him = {true, 190.1, "Broasdfsadfwn"}; //brace initializer syntax
+	Person him = {true, 190.1, "Brown", 2}; //brace initializer syntax
 	//you is an object that holds all of the member variables of a `Person` data type 
 	
-	vector<Person> listOfPeople = { him }; 
+	//cout << him.numberOfBodiesInBackyard << "\n";
 
-	cout << std::boolalpha << listOfPeople[0].hasHair << "\n";
 
+	vector<Person> listOfPeople = { him };
+
+	//cout << std::boolalpha << listOfPeople[0].hasHair << "\n";
+
+	Person me; 
+	me.eyeColor = "Blue"; 
+	me.hasHair = true; //hairists
+	me.numberOfBodiesInBackyard = 5; 
+	me.weight = 163.5; 
+
+	listOfPeople.push_back(me);
+
+	listOfPeople.push_back({ true, 123.5, "Grey", 0 }); //creating a `Person` object inplace 
+
+	cout << "Printing list of people ...\n";
+	system("pause"); 
+
+	printListOfPeople(listOfPeople);
+
+
+	//string a = 123; 
 
 
 	//the . symbol is called the "member access operator" 
